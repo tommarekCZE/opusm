1b5847c13af080ea50f0a4cfdef0c50f18b77775
tommarek <80095089+tommarekCZE@users.noreply.github.com>
2023-12-24 12:35:24 +0100

    Update packages.lua

--- sys/modules/opus/packages.lua
+++ sys/modules/opus/packages.lua
@@ -56,7 +56,7 @@ end
 
 function Packages:downloadList()
 	local packages = {
-		[ 'develop-1.8' ] = 'https://raw.githubusercontent.com/kepler155c/opus-apps/develop-1.8/packages.list',
+		[ 'develop-1.8' ] = 'https://raw.githubusercontent.com/tommarekCZE/opus-apps/develop-1.8/packages.list',
 	}
 
 	if packages[_G.OPUS_BRANCH] then
